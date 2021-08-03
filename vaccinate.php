8eb92e0e67f5ef6429964876129ac7b6733e52a5
Lamiya Tasnim <ltasnim172044@bscse.uiu.ac.bd>
2021-08-03 13:32:04 +0600

    do

---
+++ vaccinate.php
@@ -0,0 +1,19 @@
+<h1>Create Record</h1>
+
+
+
+<form method=get action=create_result.php>
+
+	NID: <input type=text name=NID> <br>
+
+	<p>
+	NO: <input type=text name=ID> <br>
+	<p>
+
+	VACCINE_DATE_1: <input type=text name=VACCINE_DATE_1> <br>
+
+	<p>
+
+	<input type=submit value=Insert>
+
+</form>
